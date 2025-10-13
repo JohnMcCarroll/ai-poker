517e2e7599f3ec1be1dbad73b48f822630c5acc8
JohnMcCarroll <jtm5356@rit.edu>
2025-10-13 18:42:29 -0400

    rendering progress

--- test_clubs_poker.py
+++ test_clubs_poker.py
@@ -3,6 +3,7 @@ import clubs
 import random
 from clubs_gym.envs import ClubsEnv
 
+
 # Define agent
 class RandomAgent(BaseAgent):
     def act(self, obs: clubs.poker.engine.ObservationDict) -> int:
@@ -13,50 +14,55 @@ class RandomAgent(BaseAgent):
             return 10
         else:
             return 100
-      
-# Instantiate heads up poker env
-env = ClubsEnv(
-    num_players=2,
-    num_streets=4,
-    blinds=[1,2],
-    antes=0,
-    raise_sizes='inf',
-    num_raises=float('inf'),
-    num_suits=4,
-    num_ranks=13,
-    num_hole_cards=2,
-    num_community_cards=[0, 3, 1, 1],
-    num_cards_for_hand=5,
-    mandatory_num_hole_cards=0,
-    start_stack=500,
-    low_end_straight=True
-    )
-env.register_agents([RandomAgent()] * 2)
-obs = env.reset()
-# env.render()
 
-# Simulate poker game
-while True:
-    bet = env.act(obs)
-    obs, rewards, done, info = env.step(bet)
-    
-    print("OBS:")
-    print(obs)
-    print()
-    print("ACTION")
-    print(bet)
-    print()
-    print("REWARDS")
-    print(rewards)
-    print()
-    print("INFO:")
-    print(info)
-    print()
-    print("DONE:")
-    
-    print(done)
 
-    if all(done):
-        break
+if __name__ == '__main__':
+    # Instantiate heads up poker env
+    env = ClubsEnv(
+        num_players=2,
+        num_streets=4,
+        blinds=[1,2],
+        antes=0,
+        raise_sizes='inf',
+        num_raises=float('inf'),
+        num_suits=4,
+        num_ranks=13,
+        num_hole_cards=2,
+        num_community_cards=[0, 3, 1, 1],
+        num_cards_for_hand=5,
+        mandatory_num_hole_cards=0,
+        start_stack=500,
+        low_end_straight=True
+        )
+    env.register_agents([RandomAgent()] * 2)
+    obs = env.reset()
+    env.render(sleep=15)
 
-print(rewards)
+    # Simulate poker game
+    while True:
+        bet = env.act(obs)
+        obs, rewards, done, info = env.step(bet)
+
+        env.render(sleep=5)
+        
+        # print("OBS:")
+        # print(obs)
+        # print()
+        # print("ACTION")
+        # print(bet)
+        # print()
+        # print("REWARDS")
+        # print(rewards)
+        # print()
+        # print("INFO:")
+        # print(info)
+        # print()
+        # print("DONE:")
+        
+        # print(done)
+
+        if all(done):
+            break
+
+    # print(rewards)
+    env.close()
